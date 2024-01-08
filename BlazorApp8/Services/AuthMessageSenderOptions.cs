c08091ebd1a292d6ba5b5cc4330f7c078461ec30
Dwayne Selsig <dselsig@hotmail.com>
2024-01-09 00:07:53 +0100

    Add project files.

---
+++ BlazorApp8/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp8.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+
+        public AuthMessageSenderOptions()
+        {
+            //skipping tutorial explaining user secrets.json https://learn.microsoft.com/en-us/aspnet/core/security/authentication/accconfirm?view=aspnetcore-8.0&tabs=visual-studio#configure-sendgrid-user-secrets
+            //hardcoding SendGridKey for now
+            SendGridKey = "";
+        }
+    }
+}
